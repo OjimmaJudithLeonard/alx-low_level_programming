eb0eea6810c3418ee11bb52e25ab34b9743595b3
OjimmaJudithLeonard <ojimmajudithleonard@gmail.com>
2023-03-16 17:13:14 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - program that prints a line using the printf function
  * Return: 0
- */
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
